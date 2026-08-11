49ccb6805d817d91cc072f642917478552942061
Shivangi1307 <2305162@kiit.ac.in>
2026-08-11 13:28:32 +0530

    Add: stone-game-ii - submission-0

---
+++ Data Structures & Algorithms/stone-game-ii/submission-0.py
@@ -0,0 +1,39 @@
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        
+        n = len(piles)
+
+        suffix = [0] * (n+1)
+
+        for i in range(n-1,-1,-1):
+            suffix[i] = piles[i] + suffix[i + 1]
+
+        dp = [[0] * (n+1) for _ in range(n)]
+
+        def solve(i,M):
+
+            if i >= n:
+                return 0
+
+            if dp[i][M] != 0:
+                return dp[i][M]
+
+            best = 0
+            for X in range(1,2*M+1):
+
+                if i + X > n:
+                    break
+
+                opponent = solve(i+X, max(X,M))
+                current = suffix[i] - opponent
+
+                best = max(best,current)
+
+            dp[i][M] = best
+
+            return best
+
+
+        return solve(0,1)
+
+        
